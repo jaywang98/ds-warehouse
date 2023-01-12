420c316c44d1bc50fa9dd1f19480160421d539dc
jaywang98 <jie.wang@my.cityu.edu.hk>
2023-01-12 20:52:00 +0800

    feat: clear redundancy

---
+++ DL/models/AlexNet/src.py
@@ -0,0 +1,10 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils import data
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from tensorboardX import SummaryWriter
+
