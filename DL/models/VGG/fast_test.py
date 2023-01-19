d4133a5601d4dea6f34fc44b764ddb1501992968
jaywang98 <jie.wang@my.cityu.edu.hk>
2023-01-19 14:50:53 +0800

    feat: add VGG, ResNet Models

---
+++ DL/models/VGG/fast_test.py
@@ -0,0 +1,237 @@
+# import libraries
+import gc
+import numpy as np
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+from sklearn.model_selection import train_test_split
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# release the memory
+torch.cuda.empty_cache()
+
+
+def data_loader(data_dir, batch_size, random_seed=42,
+                valid_size=0.1, shuffle=True, test=False):
+    gc.collect()
+    torch.cuda.empty_cache()
+    # R, G, B channels
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2023, 0.1994, 0.2010],
+    )
+
+    # Define Transforms, Compose Function zip some operation in order
+    transform = transforms.Compose([
+        transforms.Resize((227, 227)),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    # load test dataset
+    if test:
+        test_dataset = datasets.CIFAR100(
+            root=data_dir, train=False,
+            download=True, transform=transform
+        )
+
+        data_loader = torch.utils.data.DataLoader(
+            test_dataset, batch_size=batch_size, shuffle=shuffle
+        )
+
+        return data_loader
+
+    # load train datasets
+    train_dataset = datasets.CIFAR100(
+        root=data_dir, train=True,
+        download=True, transform=transform
+    )
+
+    indices = np.arange(len(train_dataset))
+    train_indices, test_indices = train_test_split(indices, train_size=3000, stratify=train_dataset.targets)
+    train_dataset = torch.utils.data.Subset(train_dataset, train_indices)
+    train_loader = torch.utils.data.DataLoader(train_dataset, shuffle=True, num_workers=16, batch_size=8)
+    valid_loader = torch.utils.data.DataLoader(train_dataset, shuffle=False, num_workers=16, batch_size=8)
+    return train_loader, valid_loader
+
+
+# CIFAR-100 dataset
+train_loader, valid_loader = data_loader(data_dir='./data', batch_size=16)
+
+test_loader = data_loader(data_dir='./data', batch_size=16, test=True)
+
+
+class VGG16(nn.Module):
+    def __init__(self, num_classes=10):
+        super(VGG16, self).__init__()
+        self.layer1 = nn.Sequential(
+            # input channel, output channel(kernel, random init)
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        # 64 dim
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU())
+        # 128 dim
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        # 256 dim
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        # 512 dim
+        self.layer9 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer13 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(7 * 7 * 512, 4096),
+            nn.ReLU())
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        self.fc2 = nn.Sequential(
+            nn.Linear(4096, num_classes))
+
+    def forward(self, x):
+        gc.collect()
+        torch.cuda.empty_cache()
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = self.layer10(out)
+        out = self.layer11(out)
+        out = self.layer12(out)
+        out = self.layer13(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
+
+num_classes = 100
+num_epochs = 10
+learning_rate = 0.005
+
+gc.collect()
+torch.cuda.empty_cache()
+model = VGG16(num_classes).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=0.005, momentum=0.9)
+
+# Train the model
+total_step = len(train_loader)
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in tqdm(enumerate(train_loader), total=total_step):
+        # Move tensors to the configured device
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        gc.collect()
+        torch.cuda.empty_cache()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        # optimizer.zero_grad()
+        for param in model.parameters():
+            param.grad = None
+
+        loss.backward()
+        optimizer.step()
+
+    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+          .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
+    # Validation
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in valid_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            gc.collect()
+            torch.cuda.empty_cache()
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
+
+        print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total))
+
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        gc.collect()
+        torch.cuda.empty_cache()
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        del images, labels, outputs
+
+    print('Accuracy of the network on the {} test images: {} %'.format(10000, 100 * correct / total))
